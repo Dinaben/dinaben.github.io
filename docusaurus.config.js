877dc37b861b3df9cd6071a9374198b90e8f3f97
Dinaben <46792559+Dinaben@users.noreply.github.com>
2023-10-19 14:54:17 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,15 +11,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://dinaben.github.io/',
+  url: 'https://dinaben.github.io/build/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/dinaben.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Dinaben', // Usually your GitHub org/user name.
-  projectName: 'build' // Usually your repo name.
+  projectName: 'dinaben' // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',

020b7f8a086426c017792224d911effab6e99ce8
Dinaben <46792559+Dinaben@users.noreply.github.com>
2023-10-19 14:55:35 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://dinaben.github.io/build/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/dinaben.github.io/',
+  baseUrl: '/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

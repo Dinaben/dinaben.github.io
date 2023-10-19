8e7a74f58dc690080dfd9a5f6055dbb1f08da351
Dinaben <46792559+Dinaben@users.noreply.github.com>
2023-10-19 14:49:05 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Dinaben', // Usually your GitHub org/user name.
-  projectName: 'dinaben.github.io', // Usually your repo name.
+  projectName: 'dinaben.github.io' // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
